e9fe99586e45e9c1ac531b9fdeabe748eb2aa04b
unicsoftware <elpidio.junior@gmail.com>
2023-11-09 18:29:19 -0300

    Porta aplicação

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({ 
   server: {
-   // port :5173,
+    port :5173,
    // host :'http://ec2-18-209-148-149.compute-1.amazonaws.com/',
     hmr: {
       overlay: false
